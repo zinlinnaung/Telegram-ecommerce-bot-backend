ee0f58ad1aab03f7e6fec8521ee7cb8f423e69f3
Zin Linn Aung <Zinlinaung4905@gmail.com>
2026-02-12 16:45:03 +0630

    updated

--- src/main.ts
+++ src/main.ts
@@ -38,7 +38,7 @@ async function bootstrap() {
   );
 
   // 5. Start the Application
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
 
   logger.log(`🚀 Application is running on: http://localhost:${port}`);
 
