daac2e26313189a2daee80c03dc887ea10d95256
Zin Linn Aung <Zinlinaung4905@gmail.com>
2026-02-20 23:48:00 +0630

    updated

--- src/bot/bot.update.ts
+++ src/bot/bot.update.ts
@@ -33,7 +33,7 @@ export const GAME_KEYBOARD = Markup.keyboard([
 @UseFilters(TelegrafExceptionFilter)
 export class BotUpdate {
   private readonly CHANNEL_ID = '-1002052753323';
-  private readonly CHANNEL_USERNAME = 'gmae_gear_mm';
+  private readonly CHANNEL_USERNAME = 'game_gear_mm';
   private readonly BONUS_AMOUNT = 3000;
   constructor(
     @InjectBot() private readonly bot: Telegraf<BotContext>,
